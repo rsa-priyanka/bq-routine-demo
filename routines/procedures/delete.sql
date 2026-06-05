bec6a8b6ff6bdcce21072ca43ed0628b3329f622
Priyanka <priyankak@rsaamerica.com>
2026-06-05 16:42:11 +0530

    UPDATE

--- routines/procedures/delete.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE PROCEDURE
-`bq-routine-demo.demo_dataset.hello_world`()
-BEGIN
-  SELECT
-    'Hello from BigQuery Procedure 2!' AS message,
-    CURRENT_TIMESTAMP() AS execution_time;
-END
